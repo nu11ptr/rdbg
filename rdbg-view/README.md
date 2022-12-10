7f2f43ff0004e79fff7fa09d0a2c035d46adbe83
Scott Meeuwsen <3615587+nu11ptr@users.noreply.github.com>
2022-12-10 11:08:02 -0500

    Add CLI and more formatting options to rdbg-view

--- rdbg-view/README.md
+++ rdbg-view/README.md
@@ -12,39 +12,32 @@ A basic command-line viewer for [rdbg](https://crates.io/crates/rdbg)
 cargo install rdbg-view
 ```
 
-## Example output
-
-The current version provides a hierarchical output, but a future version
-should offer a more traditional log viewer as well.
+## Command-line Options
 
 ```bash
+A basic command-line viewer for rdbg
+
+Usage: rdbg-view [OPTIONS] [HOSTNAME]
+
+Arguments:
+  [HOSTNAME]  Remote hostname of debugged program [default: 127.0.0.1]
+
+Options:
+  -p, --port <PORT>  Remote port on debugged program [default: 13579]
+  -d, --debug-fmt    Use debug formatting for messages (:#? formatting style)
+  -h, --help         Print help information
+  -V, --version      Print version information
+```
+
+## Example output
+
+The current version provides a typical log viewer by default, but a Rust debug
+style output (:#? formatting) is available as well with the `--debug-fmt`  flag
+
+```bash
+*** Trying to connect to 127.0.0.1:13579... ***
 *** Connected to 127.0.0.1:13579 ***
-Message {
-    time: 1670471290453,
-    thread_id: "ThreadId(1)",
-    filename: "rdbg/examples/hello_world.rs",
-    line: 4,
-    payload: Message(
-        "hello world",
-    ),
-}
-Message {
-    time: 1670471290453,
-    thread_id: "ThreadId(1)",
-    filename: "rdbg/examples/hello_world.rs",
-    line: 5,
-    payload: Values(
-        [
-            (
-                "world",
-                "\"world\"",
-            ),
-            (
-                "1 + 5",
-                "6",
-            ),
-        ],
-    ),
-}
+T:1670688040648 THR:1 rdbg/examples/hello_world.rs:4 hello world
+T:1670688040648 THR:1 rdbg/examples/hello_world.rs:5 |world->"world"| |1 + 5->6|
 *** Disconnected from 127.0.0.1:13579 ***
 ```
--- rdbg-view/src/main.rs
+++ rdbg-view/src/main.rs
@@ -1,7 +1,28 @@
-use rdbg_client::{Error, Event, MsgIterator, DEFAULT_ADDR, DEFAULT_PORT};
+use clap::Parser;
+use rdbg_client::{Error, Event, Message, MsgIterator, MsgPayload, DEFAULT_ADDR, DEFAULT_PORT};
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Remote hostname of debugged program
+    #[arg(default_value_t = DEFAULT_ADDR.to_string())]
+    hostname: String,
+
+    /// Remote port on debugged program
+    #[arg(short, long, default_value_t = DEFAULT_PORT)]
+    port: u16,
+
+    /// Use debug formatting for messages (:#? formatting style)
+    #[arg(short, long, default_value_t = false)]
+    debug_fmt: bool,
+}
 
 fn main() {
-    eprintln!("*** Trying to connect to {DEFAULT_ADDR}:{DEFAULT_PORT}... ***");
+    let args = Args::parse();
+    eprintln!(
+        "*** Trying to connect to {}:{}... ***",
+        &args.hostname, args.port
+    );
 
     let iterator = MsgIterator::default();
 
@@ -15,7 +36,11 @@ fn main() {
                     eprintln!("*** Disconnected from {addr} ***");
                 }
                 Event::Message(msg) => {
-                    println!("{msg:#?}");
+                    if args.debug_fmt {
+                        println!("{msg:#?}");
+                    } else {
+                        print_message(&msg);
+                    }
                 }
             },
             Err(err) => match err {
@@ -34,4 +59,28 @@ fn main() {
     }
 
     eprintln!("*** Exiting... ***");
+}
+
+fn print_message(msg: &Message) {
+    print!(
+        "T:{} THR:{} {}:{}",
+        msg.time, msg.thread_id, msg.filename, msg.line
+    );
+
+    match &msg.payload {
+        MsgPayload::Message(msg) => {
+            // If it contains a newline (but not at end) then move to next line to give alignment
+            if !msg.is_empty() && msg[..msg.len()].contains('\n') {
+                println!();
+            }
+
+            println!(" {}", msg);
+        }
+        MsgPayload::Values(values) => {
+            for (key, value) in values {
+                print!(" |{key}->{value}|")
+            }
+            println!();
+        }
+    }
 }
